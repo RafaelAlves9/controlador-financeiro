e2a94376f4b34037d06f53dbbffdb75031234a26
Rafael Alves <hrafael.alves9@gmail.com>
2022-04-20 10:02:18 -0300

    up

---
+++ src/data/categories.ts
@@ -0,0 +1,6 @@
+import { Category } from '../types/Category';
+
+export const categories: Category = {
+    food: { title: 'Saída', color: 'red', expense: true },
+    salary: { title: 'Entrada', color: 'green', expense: false }
+}
